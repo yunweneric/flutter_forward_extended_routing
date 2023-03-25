d39d86a572c42122350590c7d02b36cdbfd40e26
e.yunwen <e.yunwen@commonfactor.tech>
2023-03-25 12:57:00 +0100

    implementing bottom app bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,30 +11,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.teal,
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              "Homepage",
-              style: TextStyle(color: Colors.white, fontSize: 30),
-            ),
-            SizedBox(height: 20),
-            appButton(
-              context: context,
-              width: 200,
-              onTap: () => Navigator.pushNamed(context, AppRoutes.recipes),
-              text: "Route to Recipes",
-            )
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) => setState(() {
+          selectedIndex = value;
+        }),
+        currentIndex: selectedIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.linked_camera), label: "Camera"),
+        ],
       ),
+      body: selectedIndex == 0
+          ? Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height,
+              color: Colors.teal,
+              child: Center(
+                child: Text(
+                  "First Page",
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ),
+              ),
+            )
+          : Container(
+              color: Colors.teal,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Homepage",
+                    style: TextStyle(color: Colors.white, fontSize: 30),
+                  ),
+                  SizedBox(height: 20),
+                  appButton(
+                    context: context,
+                    width: 200,
+                    onTap: () => Navigator.pushNamed(context, AppRoutes.recipes),
+                    text: "Route to Recipes",
+                  )
+                ],
+              ),
+            ),
     );
   }
 }
